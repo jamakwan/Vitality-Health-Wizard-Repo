1d1471b339ef8659744e896aca35a66fba2331d7
System Administrator <root@Jaineels-MacBook-Pro.local>
2023-05-16 08:16:44 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Vitality-Health-Wizard',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
