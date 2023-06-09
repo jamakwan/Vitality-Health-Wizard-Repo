be0199dc85a79bd0bef29c634f86ea3de88f54ce
Shailesh Desai <shaileshdesai574@gmail.com>
2023-06-08 17:17:34 -0700

    added access to storage on health-score page

--- src/app/health-score/health-score.page.ts
+++ src/app/health-score/health-score.page.ts
@@ -13,3 +13,28 @@ export class HealthScorePage implements OnInit {
   }
 
 }
+
+var cals = [-1];
+if(localStorage.getItem("cals") != null){
+  // @ts-ignore we checked above it was null!
+  cals = JSON.parse(localStorage.getItem("cals"));
+}
+else{
+  cals.pop();
+}
+var calsBurned = [-1];
+if(localStorage.getItem("calsBurned") != null){
+  // @ts-ignore we checked above it was null!
+  calsBurned = JSON.parse(localStorage.getItem("calsBurned"));
+}
+else{
+  calsBurned.pop();
+}
+var sleep = [-1];
+if(localStorage.getItem("sleep") != null){
+  // @ts-ignore we checked above it was null!
+  cals = JSON.parse(localStorage.getItem("sleep"));
+}
+else{
+  sleep.pop();
+}
